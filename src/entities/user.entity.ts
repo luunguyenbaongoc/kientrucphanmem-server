2e1b87b8395e0e5075363ac2133fa6fe6adc3ff2
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2024-09-23 11:36:08 +0700

    add user entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  createddate: Date;
+
+  // @Column({ type: 'jsonb', default: [] })
+  // refreshtokenlist: string[];
+
+  @Column()
+  active: boolean;
+}
