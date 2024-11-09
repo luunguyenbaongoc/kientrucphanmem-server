5821713291bed329b3cbfec6aa38811446f5c287
Tiensinh98 <phamtiensinh010@gmail.com>
2024-11-09 19:39:37 +0700

    make more dto properties as ApiProperty

--- src/modules/auth/dto/resset-password.dto.ts
+++ src/modules/auth/dto/resset-password.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class ResetPasswordDto {
+  @ApiProperty()
   @IsString({ message: 'Không có số điện thoại' })
   phone: string;
 
+  @ApiProperty()
   @IsString({ message: 'Không có mật khẩu' })
   new_password: string;
 }
