cd31af57b95e7973260461583b793173eb30f8b4
Tiensinh98 <phamtiensinh010@gmail.com>
2024-11-16 01:08:30 +0700

    allow upload avatar for group

--- src/modules/group/group.controller.ts
+++ src/modules/group/group.controller.ts
@@ -5,12 +5,25 @@ import {
   Post,
   Body,
   Put,
-  Get,
+  UseGuards,
+  UploadedFile,
+  UseInterceptors,
+  Param
 } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
+import { 
+  ApiTags, 
+  ApiBearerAuth, 
+  ApiConsumes,
+  ApiBody
+} from '@nestjs/swagger';
 import { GroupService } from './group.service';
 import { AuthUser } from 'src/decorators';
 import { AddGroupDto, UpdateGroupDto } from './dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/guards';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Group } from 'src/entities';
 
 @ApiTags('Group')
 @ApiBearerAuth()
@@ -33,6 +46,48 @@ export class GroupController {
     @Body() updateGroupDto: UpdateGroupDto,
   ) {
     return this.groupService.updateGroup(userId, updateGroupDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          callback(null, `${uniqueSuffix}${extname(file.originalname)}`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        const isImage = ['image/png', 'image/jpeg', 'image/jpg'].includes(file.mimetype);
+        if (!isImage) {
+          return callback(new Error(`Không hỗ trợ file ${extname(file.originalname)}`), false);
+        }
+        callback(null, true);
+      }
+    }),
+  )
+  @Post('/:groupId/upload-image')
+  @UseGuards(JwtAuthGuard)
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  async uploadGroupAvatar(
+    @AuthUser() userId: string,
+    @UploadedFile() file: Express.Multer.File,
+    @Param('groupId') groupId: string,
+  ): Promise<Group> {
+    return this.groupService.updateGroup(userId, { id: groupId, avatar: file.path } as UpdateGroupDto);
   }
 
   // @HttpCode(HttpStatus.NO_CONTENT)
